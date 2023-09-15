76c0b62f8fd5f9f293f9849552308ec678ec1861
lins <lins@keyvalue.systems>
2023-09-15 12:28:17 +0530

    feat : Add navbar

--- src/container/Dashboard.jsx
+++ src/container/Dashboard.jsx
@@ -1,8 +1,12 @@
+import Navbar from "./Navbar";
+
 const Dashboard = () => {
   return (
-    <h1> Dashboard </h1>
+    <div className="flex">
+        <Navbar />   
+      <div className="h-[100vh] bg-[#F4F3FA] flex-1">Dashboard</div>
+    </div>
   );
-}
+};
 
 export default Dashboard;
-
